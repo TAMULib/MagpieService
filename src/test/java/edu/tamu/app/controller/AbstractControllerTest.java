ea500c6ffd9fb451ab781eed27a5b6d80e13babe
Rincy Mathew <rmathew@tamu.edu>
2017-03-30 09:50:51 -0500

    UserControllerTest added

--- src/test/java/edu/tamu/app/controller/AbstractControllerTest.java
+++ src/test/java/edu/tamu/app/controller/AbstractControllerTest.java
@@ -1,33 +1,49 @@
 package edu.tamu.app.controller;
 
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.HashMap;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.tamu.app.WebServerInit;
+import edu.tamu.app.model.AppUser;
+import edu.tamu.app.model.repo.AppUserRepo;
+import edu.tamu.app.service.DocumentPushService;
 import edu.tamu.app.service.ProjectsService;
 import edu.tamu.app.service.SyncService;
+import edu.tamu.app.service.VoyagerService;
+import edu.tamu.framework.model.ApiResponse;
 
+@ActiveProfiles("test")
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WebServerInit.class)
 public abstract class AbstractControllerTest extends MockData {
 
+	protected static ApiResponse response;
+
 	@Mock
+	protected AppUserRepo userRepo;
+
+	@Spy
 	protected ObjectMapper objectMapper;
 
 	@Mock
@@ -39,15 +55,54 @@ public abstract class AbstractControllerTest extends MockData {
 	@Mock
 	protected ResourceLoader resourceLoader;
 
+	@Mock
+	protected DocumentPushService documentPushService;
+
+	@Mock
+	protected VoyagerService voyagerService;
+
+	@Mock
+	protected SimpMessagingTemplate simpMessagingTemplate;
+
 	@InjectMocks
 	protected AdminController adminController;
 
 	@InjectMocks
 	protected ControlledVocabularyController controlledVocabularyController;
 
+	@InjectMocks
+	protected DocumentController documentController;
+
+	@InjectMocks
+	protected MetadataController metadataController;
+
+	@InjectMocks
+	protected ProjectController projectController;
+
+	@InjectMocks
+	protected UserController userController;
+
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
+
+		when(userRepo.findAll()).thenReturn(mockAppUserList);
+
+		when(userRepo.save(any (AppUser.class))).then( new Answer<AppUser>() {
+			@Override
+			public AppUser answer(InvocationOnMock invocation) throws Throwable {
+				return saveAppUser((AppUser)invocation.getArguments()[0]);
+			}
+		});
+
+		aggieJackToken = new HashMap<String, String>();
+		aggieJackToken.put("lastName","Daniels");
+		aggieJackToken.put("firstName","Jack");
+		aggieJackToken.put("netid","aggiejack");
+		aggieJackToken.put("uin","123456789");
+		aggieJackToken.put("exp",null);
+		aggieJackToken.put("email","aggiejack@tamu.edu");
+
 		grantorList.add("A & M");
 		grantorList.add("TAMU");
 
@@ -58,16 +113,16 @@ public abstract class AbstractControllerTest extends MockData {
 		cvMap.put("grantor", grantorList);
 		cvMap.put("degrees", degreeList);
 
-		when(objectMapper.readValue(any (String.class), any (TypeReference.class))).then(new Answer<Map<String, Object>>(){
-			@Override
-			public Map<String, Object> answer(InvocationOnMock invocation) throws Throwable {
-				return cvMap;
-			}
-		});
+//		when(objectMapper.readValue(any (String.class), any (TypeReference.class))).then(new Answer<Map<String, Object>>(){
+//			@Override
+//			public Map<String, Object> answer(InvocationOnMock invocation) throws Throwable {
+//				return cvMap;
+//			}
+//		});
 	}
 
 	@After
 	public void cleanUp () {
-		
+		response = null;
 	}
 }
--- src/test/java/edu/tamu/app/controller/MockData.java
+++ src/test/java/edu/tamu/app/controller/MockData.java
@@ -1,17 +1,55 @@
 package edu.tamu.app.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import edu.tamu.app.enums.AppRole;
+import edu.tamu.app.model.AppUser;
 import edu.tamu.framework.model.ApiResponse;
+import edu.tamu.framework.model.Credentials;
 
 public class MockData {
 
 	protected static ApiResponse response;
 
-	//ControlledVocabulary
+	protected static Credentials credentials = new Credentials();
+
+	protected static AppUser TEST_USER1 = new AppUser(123456789l, "Jane", "Daniel", AppRole.ROLE_ADMIN.toString());
+	protected static AppUser TEST_USER2 = new AppUser(234567891l, "Jane", "Daniel", AppRole.ROLE_MANAGER.toString());
+	protected static AppUser TEST_USER3 = new AppUser(345678912l, "Jane", "Daniel", AppRole.ROLE_USER.toString());
+	protected static List<AppUser> mockAppUserList = new ArrayList<AppUser>(Arrays.asList(new AppUser[] {TEST_USER1 , TEST_USER2 , TEST_USER3}));
+
+	static {
+		TEST_USER1.setId(1l);
+		TEST_USER2.setId(2l);
+		TEST_USER3.setId(3l);
+		//credentials
+		credentials.setAffiliation("TAMU_LIB");
+		credentials.setFirstName(TEST_USER1.getFirstName());
+		credentials.setLastName(TEST_USER1.getLastName());
+		credentials.setEmail("aggiejack@tamu.edu");
+		credentials.setNetid("aggiejack");
+		credentials.setRole("ROLE_USER");
+		credentials.setUin(TEST_USER1.getUin().toString());
+	}
+
+	public AppUser saveAppUser(AppUser modifiedUser) {
+		for(AppUser user : mockAppUserList) {
+			if(user.getUin().equals(modifiedUser.getUin())) {
+				user.setFirstName(modifiedUser.getFirstName());
+				user.setLastName(modifiedUser.getLastName());
+				user.setRole(modifiedUser.getRole());
+				return user;
+			}
+		}
+		return null;
+	}
+
+	protected static Map<String, String> aggieJackToken;
+
 	protected static List<String> grantorList = new ArrayList<String>();
 	protected static List<String> degreeList = new ArrayList<String>();
 
