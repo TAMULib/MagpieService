838786a006e6b805816e942d6747fe597d7c8ede
William Welling <wwelling@library.tamu.edu>
2017-06-22 11:10:09 -0500

    formatted FlatMarc

--- src/main/java/edu/tamu/app/model/response/marc/FlatMARC.java
+++ src/main/java/edu/tamu/app/model/response/marc/FlatMARC.java
@@ -4,247 +4,226 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlatMARC {
-	
-	private String dc_creator = "";
-	private String dc_title = "";
-	private String dc_date_created = "";
-	private String dc_date_issued = "";
-	private List<String> dc_subject_lcsh = new ArrayList<String>();
-	private String dc_subject = "";
-	private String dc_description = "";
-	private String dc_description_abstract = "";
-	private String thesis_degree_grantor = "";
-	
-	public FlatMARC(VoyagerServiceData voyagerServiceData) {
-		
-		if(voyagerServiceData.getServiceData() != null) {
-			
-			Datafield[] dataField = voyagerServiceData.getServiceData().getHoldingsRecord().getBibRecord().getMarcRecord().getDatafield();
-			
-			for(Datafield df : dataField) {
-				
-				// dc.creator
-				if(df.getTag().equals("100")) {
-					Subfield[] subFields = df.getSubfield();
-					if(subFields.length > 0) {
-						dc_creator = scrubField(".", subFields[0].getValue());
-					}
-				}
-				
-				// dc.title
-				if(df.getTag().equals("245")) {
-					Subfield[] subFields = df.getSubfield();
-					for(Subfield subField : subFields) {
-						if(subField.getCode().equals("a") || subField.getCode().equals("b")) {
-							dc_title += scrubField(".", subField.getValue());
-						}
-					}
-				}
-				
-				// dc.date.created and dc.date.issued
-				if(df.getTag().equals("260")) {
-					Subfield[] subFields = df.getSubfield();
-					for(Subfield subField : subFields) {
-						if(subField.getCode().equals("c")) {
-							dc_date_issued = dc_date_created = scrubField(".", subField.getValue());
-						}
-					}
-				}
-				
-				// dc.date.created and dc.date.issued
-				if(df.getTag().equals("264")) {
-					if(dc_date_issued.equals("")) {
-						Subfield[] subFields = df.getSubfield();
-						for(Subfield subField : subFields) {
-							if(df.getInd2().equals("0") || df.getInd2().equals("1")) {
-								dc_date_issued = dc_date_created = scrubField(".", subField.getValue());
-							}
-						}					
-					}
-				}
-				
-				// dc.description
-				if(df.getTag().equals("500") || df.getTag().equals("502")) {
-					Subfield[] subFields = df.getSubfield();
-					for(Subfield subField : subFields) {
-						if(subField.getCode().equals("a") || subField.getCode().equals("b")) {
-							if(dc_description.length() > 0) {
-								System.out.println("Multiple description found. Deferring to the first.");
-								System.out.println(scrubField(".", subField.getValue()));
-								continue;
-							}
-							dc_description += scrubField(".", subField.getValue());
-						}
-					}
-				}
-				
-				// thesis.degree.grantor
-				if(df.getTag().equals("502")) {
-					Subfield[] subFields = df.getSubfield();
-					for(Subfield subField : subFields) {
-						if(subField.getCode().equals("c")) {
-							thesis_degree_grantor += scrubField(".", subField.getValue());
-							if(dc_description.length() > 0) {
-								dc_description += " -- " + thesis_degree_grantor;
-							}
-						}
-					}
-				}
-				
-				// dc.description.abstract
-				if(df.getTag().equals("520")) {
-					Subfield[] subFields = df.getSubfield();
-					for(Subfield subField : subFields) {						
-						dc_description_abstract += subField.getValue();
-					}
-				}
-				
-				// dc.subject.lcsh and dc.subject
-				if(df.getTag().equals("600") || df.getTag().equals("610") || df.getTag().equals("611") || df.getTag().equals("630") || df.getTag().equals("650") || df.getTag().equals("651")) {
-					Subfield[] subFields = df.getSubfield();					
-					String lcsh = "";					
-					for(Subfield subField : subFields) {						
-						if(df.getInd2().equals("4")) {
-							dc_subject += scrubField(".", subField.getValue());
-						}
-						else if(df.getInd2().equals("0")) {
-							lcsh += subField.getValue();
-						}					
-						if(subField.getCode().equals("x") || subField.getCode().equals("z")) {
-							if(lcsh.length() > 0) {
-								lcsh += " -- " + subField.getValue();
-							}
-							else {
-								lcsh += subField.getValue();
-							}
-							
-						}					
-					}
-					if(!"".equals(lcsh)) {
-						dc_subject_lcsh.add(lcsh);
-					}	
-				}
-				
-				// dc.subject
-				if(df.getTag().equals("653")) {
-					Subfield[] subFields = df.getSubfield();
-					for(Subfield subField : subFields) {
-						if(dc_subject.length() > 0) {
-							dc_subject += ", " + subField.getValue();
-						}
-						else {
-							dc_subject += subField.getValue();
-						}
-						
-					}
-				}
-				
-			}
-		
-		}
-		
-	}
 
+    private String dc_creator = "";
+    private String dc_title = "";
+    private String dc_date_created = "";
+    private String dc_date_issued = "";
+    private List<String> dc_subject_lcsh = new ArrayList<String>();
+    private String dc_subject = "";
+    private String dc_description = "";
+    private String dc_description_abstract = "";
+    private String thesis_degree_grantor = "";
 
-	public String getCreator() {
-		return dc_creator;
-	}
+    public FlatMARC(VoyagerServiceData voyagerServiceData) {
 
+        if (voyagerServiceData.getServiceData() != null) {
 
-	public void setCreator(String dc_creator) {
-		this.dc_creator = dc_creator;
-	}
+            Datafield[] dataField = voyagerServiceData.getServiceData().getHoldingsRecord().getBibRecord().getMarcRecord().getDatafield();
 
+            for (Datafield df : dataField) {
 
-	public String getTitle() {
-		return dc_title;
-	}
+                // dc.creator
+                if (df.getTag().equals("100")) {
+                    Subfield[] subFields = df.getSubfield();
+                    if (subFields.length > 0) {
+                        dc_creator = scrubField(".", subFields[0].getValue());
+                    }
+                }
 
+                // dc.title
+                if (df.getTag().equals("245")) {
+                    Subfield[] subFields = df.getSubfield();
+                    for (Subfield subField : subFields) {
+                        if (subField.getCode().equals("a") || subField.getCode().equals("b")) {
+                            dc_title += scrubField(".", subField.getValue());
+                        }
+                    }
+                }
 
-	public void setTitle(String dc_title) {
-		this.dc_title = dc_title;
-	}
+                // dc.date.created and dc.date.issued
+                if (df.getTag().equals("260")) {
+                    Subfield[] subFields = df.getSubfield();
+                    for (Subfield subField : subFields) {
+                        if (subField.getCode().equals("c")) {
+                            dc_date_issued = dc_date_created = scrubField(".", subField.getValue());
+                        }
+                    }
+                }
 
+                // dc.date.created and dc.date.issued
+                if (df.getTag().equals("264")) {
+                    if (dc_date_issued.equals("")) {
+                        Subfield[] subFields = df.getSubfield();
+                        for (Subfield subField : subFields) {
+                            if (df.getInd2().equals("0") || df.getInd2().equals("1")) {
+                                dc_date_issued = dc_date_created = scrubField(".", subField.getValue());
+                            }
+                        }
+                    }
+                }
 
-	public String getDateCreated() {
-		return dc_date_created;
-	}
+                // dc.description
+                if (df.getTag().equals("500") || df.getTag().equals("502")) {
+                    Subfield[] subFields = df.getSubfield();
+                    for (Subfield subField : subFields) {
+                        if (subField.getCode().equals("a") || subField.getCode().equals("b")) {
+                            if (dc_description.length() > 0) {
+                                System.out.println("Multiple description found. Deferring to the first.");
+                                System.out.println(scrubField(".", subField.getValue()));
+                                continue;
+                            }
+                            dc_description += scrubField(".", subField.getValue());
+                        }
+                    }
+                }
 
+                // thesis.degree.grantor
+                if (df.getTag().equals("502")) {
+                    Subfield[] subFields = df.getSubfield();
+                    for (Subfield subField : subFields) {
+                        if (subField.getCode().equals("c")) {
+                            thesis_degree_grantor += scrubField(".", subField.getValue());
+                            if (dc_description.length() > 0) {
+                                dc_description += " -- " + thesis_degree_grantor;
+                            }
+                        }
+                    }
+                }
 
-	public void setDateCreated(String dc_date_created) {
-		this.dc_date_created = dc_date_created;
-	}
+                // dc.description.abstract
+                if (df.getTag().equals("520")) {
+                    Subfield[] subFields = df.getSubfield();
+                    for (Subfield subField : subFields) {
+                        dc_description_abstract += subField.getValue();
+                    }
+                }
 
+                // dc.subject.lcsh and dc.subject
+                if (df.getTag().equals("600") || df.getTag().equals("610") || df.getTag().equals("611") || df.getTag().equals("630") || df.getTag().equals("650") || df.getTag().equals("651")) {
+                    Subfield[] subFields = df.getSubfield();
+                    String lcsh = "";
+                    for (Subfield subField : subFields) {
+                        if (df.getInd2().equals("4")) {
+                            dc_subject += scrubField(".", subField.getValue());
+                        } else if (df.getInd2().equals("0")) {
+                            lcsh += subField.getValue();
+                        }
+                        if (subField.getCode().equals("x") || subField.getCode().equals("z")) {
+                            if (lcsh.length() > 0) {
+                                lcsh += " -- " + subField.getValue();
+                            } else {
+                                lcsh += subField.getValue();
+                            }
 
-	public String getDateIssued() {
-		return dc_date_issued;
-	}
+                        }
+                    }
+                    if (!"".equals(lcsh)) {
+                        dc_subject_lcsh.add(lcsh);
+                    }
+                }
 
+                // dc.subject
+                if (df.getTag().equals("653")) {
+                    Subfield[] subFields = df.getSubfield();
+                    for (Subfield subField : subFields) {
+                        if (dc_subject.length() > 0) {
+                            dc_subject += ", " + subField.getValue();
+                        } else {
+                            dc_subject += subField.getValue();
+                        }
 
-	public void setDateIssued(String dc_date_issued) {
-		this.dc_date_issued = dc_date_issued;
-	}
+                    }
+                }
 
+            }
 
-	public List<String> getSubjectIcsh() {
-		return dc_subject_lcsh;
-	}
+        }
 
+    }
 
-	public void setSubjectIcsh(List<String> dc_subject_lcsh) {
-		this.dc_subject_lcsh = dc_subject_lcsh;
-	}
+    public String getCreator() {
+        return dc_creator;
+    }
 
+    public void setCreator(String dc_creator) {
+        this.dc_creator = dc_creator;
+    }
 
-	public String getSubject() {
-		return dc_subject;
-	}
+    public String getTitle() {
+        return dc_title;
+    }
 
+    public void setTitle(String dc_title) {
+        this.dc_title = dc_title;
+    }
 
-	public void setSubject(String dc_subject) {
-		this.dc_subject = dc_subject;
-	}
+    public String getDateCreated() {
+        return dc_date_created;
+    }
 
+    public void setDateCreated(String dc_date_created) {
+        this.dc_date_created = dc_date_created;
+    }
 
-	public String getDescription() {
-		return dc_description;
-	}
+    public String getDateIssued() {
+        return dc_date_issued;
+    }
 
+    public void setDateIssued(String dc_date_issued) {
+        this.dc_date_issued = dc_date_issued;
+    }
 
-	public void setDescription(String dc_description) {
-		this.dc_description = dc_description;
-	}
+    public List<String> getSubjectIcsh() {
+        return dc_subject_lcsh;
+    }
 
+    public void setSubjectIcsh(List<String> dc_subject_lcsh) {
+        this.dc_subject_lcsh = dc_subject_lcsh;
+    }
 
-	public String getDescriptionAbstract() {
-		return dc_description_abstract;
-	}
+    public String getSubject() {
+        return dc_subject;
+    }
 
+    public void setSubject(String dc_subject) {
+        this.dc_subject = dc_subject;
+    }
 
-	public void setDescriptionAbstract(String dc_description_abstract) {
-		this.dc_description_abstract = dc_description_abstract;
-	}
+    public String getDescription() {
+        return dc_description;
+    }
 
+    public void setDescription(String dc_description) {
+        this.dc_description = dc_description;
+    }
 
-	public String getDegreeGrantor() {
-		return thesis_degree_grantor;
-	}
+    public String getDescriptionAbstract() {
+        return dc_description_abstract;
+    }
 
+    public void setDescriptionAbstract(String dc_description_abstract) {
+        this.dc_description_abstract = dc_description_abstract;
+    }
 
-	public void setDegreeGrantor(String thesis_degree_grantor) {
-		this.thesis_degree_grantor = thesis_degree_grantor;
-	}
-	
-	private String scrubField(String scrubber, String scrubbable) {
-		scrubbable = scrubbable.trim();
-		if (scrubbable.endsWith(scrubber)) {
-			return rightTrim(scrubber.length(), scrubbable);
-		}
-		return scrubbable;
-	}
-	
-	private String rightTrim(int length, String trimmable) {
-		return trimmable.substring(0, trimmable.length() - length);
-	}
+    public String getDegreeGrantor() {
+        return thesis_degree_grantor;
+    }
+
+    public void setDegreeGrantor(String thesis_degree_grantor) {
+        this.thesis_degree_grantor = thesis_degree_grantor;
+    }
+
+    private String scrubField(String scrubber, String scrubbable) {
+        scrubbable = scrubbable.trim();
+        if (scrubbable.endsWith(scrubber)) {
+            return rightTrim(scrubber.length(), scrubbable);
+        }
+        return scrubbable;
+    }
+
+    private String rightTrim(int length, String trimmable) {
+        return trimmable.substring(0, trimmable.length() - length);
+    }
 
 }
