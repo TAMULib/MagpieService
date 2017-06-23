eb7349f9b96e445b86111c2196ca8db6f11156e7
William Welling <wwelling@library.tamu.edu>
2017-06-23 16:08:18 -0500

    added todo in ServiceType enum

--- src/main/java/edu/tamu/app/enums/ServiceType.java
+++ src/main/java/edu/tamu/app/enums/ServiceType.java
@@ -1,5 +1,6 @@
 package edu.tamu.app.enums;
 
+// TODO: restrict to general service, FEDORA is the service type the repository bean should be configured by repository name
 public enum ServiceType {
     DSPACE, VOYAGER, CSV, NALT, FEDORA_SPOTLIGHT, ARCHIVEMATICA, FEDORA_PCDM
 }
