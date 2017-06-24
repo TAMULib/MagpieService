15623daf7552383f032ba340b237e27167c3cd53
William Welling <wwelling@library.tamu.edu>
2017-06-24 01:55:37 -0500

    added resource entity

---
+++ src/main/java/edu/tamu/app/model/Resource.java
@@ -0,0 +1,67 @@
+package edu.tamu.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import edu.tamu.framework.model.BaseEntity;
+
+@Entity
+public class Resource extends BaseEntity {
+
+	@Column(nullable = false)
+    private String name;
+
+	@Column(nullable = false)
+    private String path;
+
+	@Column(nullable = false)
+    private String url;
+
+	@Column(nullable = false)
+    private String mimeType;
+	
+	public Resource() {
+		
+	}
+
+	public Resource(String name, String path, String url, String mimeType) {
+		this();
+		this.name = name;
+		this.path = path;		
+		this.url = url;
+		this.mimeType = mimeType;
+	}
+	
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+}
