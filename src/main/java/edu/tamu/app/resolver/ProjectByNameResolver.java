c06f3db4a47b2c4fd467b66e4f1e064871b3404f
William Welling <wwelling@library.tamu.edu>
2017-12-01 14:21:09 -0600

    using abstract resolver from framework

--- src/main/java/edu/tamu/app/resolver/ProjectByNameResolver.java
+++ src/main/java/edu/tamu/app/resolver/ProjectByNameResolver.java
@@ -1,51 +1,15 @@
 package edu.tamu.app.resolver;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.annotation.ObjectIdGenerator;
-import com.fasterxml.jackson.annotation.ObjectIdResolver;
+import edu.tamu.weaver.data.resolver.EntityByPropertyResolver;
 
 @Component
-public class ProjectByNameResolver implements ObjectIdResolver {
-
-    private static final String COLUMN_NAME = "name";
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public ProjectByNameResolver() {
-
-    }
+public class ProjectByNameResolver extends EntityByPropertyResolver {
 
     @Override
-    public void bindItem(ObjectIdGenerator.IdKey id, Object ob) {
-    }
-
-    @Override
-    public Object resolveId(final ObjectIdGenerator.IdKey idKey) {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Object> query = cb.createQuery();
-        Root<?> e = query.from(idKey.scope);
-        query.select(e).distinct(true);
-        query.where(cb.equal(e.get(COLUMN_NAME), idKey.key));
-        Object entity = entityManager.createQuery(query).getSingleResult();
-        return entity;
-    }
-
-    @Override
-    public boolean canUseFor(ObjectIdResolver resolverType) {
-        return getClass().isAssignableFrom(resolverType.getClass());
-    }
-
-    @Override
-    public ObjectIdResolver newForDeserialization(Object c) {
-        return this;
+    protected String getPropertyName() {
+        return "name";
     }
 
 }
