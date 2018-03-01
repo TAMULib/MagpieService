d9ff5bd98c810eabcaf956a3383eabcf8245bcd6
William Welling <wwelling@library.tamu.edu>
2018-03-01 09:04:51 -0600

    abstraction for document listener

---
+++ src/main/java/edu/tamu/app/observer/AbstractDocumentListener.java
@@ -0,0 +1,122 @@
+package edu.tamu.app.observer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.io.monitor.FileAlterationObserver;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.tamu.app.exception.DocumentNotFoundException;
+import edu.tamu.app.model.Document;
+import edu.tamu.app.service.DocumentFactory;
+
+public abstract class AbstractDocumentListener extends AbstractFileListener {
+
+    private static final Logger logger = Logger.getLogger(AbstractDocumentListener.class);
+
+    protected static final Map<String, List<String>> pendingResources = new ConcurrentHashMap<String, List<String>>();
+
+    protected static final ExecutorService executor = Executors.newFixedThreadPool(10);
+
+    @Autowired
+    protected DocumentFactory documentFactory;
+
+    public AbstractDocumentListener(String root, String folder) {
+        super(root, folder);
+    }
+
+    @Override
+    public void onStart(FileAlterationObserver observer) {
+
+    }
+
+    @Override
+    public void onDirectoryCreate(File directory) {
+        initializePendingResources(directory.getName());
+        createDocument(directory).thenAccept(document -> {
+            if (document != null) {
+                processPendingResources(document);
+            } else {
+                logger.warn("Unable to create document!");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDirectoryChange(File directory) {
+
+    }
+
+    @Override
+    public void onDirectoryDelete(File directory) {
+
+    }
+
+    @Override
+    public void onFileCreate(File file) {
+        if (!file.isHidden() && file.isFile()) {
+            logger.info("File created: " + file.getName());
+            try {
+                addResource(file);
+            } catch (DocumentNotFoundException e) {
+                String documentName = file.getParentFile().getName();
+                logger.info("Document " + documentName + " not yet created for resource. Adding to pending resources.");
+                List<String> documentPendingResources = pendingResources.get(documentName);
+                documentPendingResources.add(file.getAbsolutePath());
+            }
+        }
+    }
+
+    @Override
+    public void onFileChange(File file) {
+
+    }
+
+    @Override
+    public void onFileDelete(File file) {
+
+    }
+
+    @Override
+    public void onStop(FileAlterationObserver observer) {
+
+    }
+
+    protected void initializePendingResources(String documentName) {
+        pendingResources.put(documentName, new ArrayList<String>());
+    }
+
+    protected Document processPendingResources(Document document) {
+        String documentName = document.getName();
+        for (String resourcePath : pendingResources.get(documentName)) {
+            document = documentFactory.addResource(document, new File(resourcePath));
+        }
+        pendingResources.remove(documentName);
+        return document;
+    }
+
+    protected CompletableFuture<Document> createDocument(File directory) {
+        return CompletableFuture.supplyAsync(() -> {
+            Document document = null;
+            try {
+                document = documentFactory.createDocument(directory);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return document;
+        }, executor);
+    }
+
+    protected void addResource(File file) throws DocumentNotFoundException {
+        documentFactory.addResource(file);
+    }
+
+}
---
+++ src/main/java/edu/tamu/app/observer/HeadlessDocumentListener.java
@@ -0,0 +1,98 @@
+package edu.tamu.app.observer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import edu.tamu.app.model.Document;
+import edu.tamu.app.model.MetadataFieldGroup;
+import edu.tamu.app.model.MetadataFieldValue;
+import edu.tamu.app.model.ProjectRepository;
+import edu.tamu.app.model.repo.DocumentRepo;
+import edu.tamu.app.service.registry.MagpieServiceRegistry;
+import edu.tamu.app.service.repository.Repository;
+
+public class HeadlessDocumentListener extends AbstractDocumentListener {
+
+    private static final Logger logger = Logger.getLogger(HeadlessDocumentListener.class);
+
+    @Value("${app.document.create.wait}")
+    private int documentCreationWait;
+
+    @Autowired
+    private DocumentRepo documentRepo;
+
+    @Autowired
+    private MagpieServiceRegistry projectServiceRegistry;
+
+    public HeadlessDocumentListener(String root, String folder) {
+        super(root, folder);
+    }
+
+    protected CompletableFuture<Document> createDocument(File directory) {
+        return CompletableFuture.supplyAsync(() -> {
+            Document document = null;
+            try {
+                initializePendingResources(directory.getName());
+                waitOnDirectory(directory);
+                document = documentFactory.createDocument(directory);
+                document = processPendingResources(document);
+                document = applyDefaultValues(document);
+                document = publishToRepositories(document);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return document;
+        }, executor);
+    }
+
+    // this is a blocking sleep operation of this listener
+    private void waitOnDirectory(File directory) {
+        logger.info("Waiting for directory " + directory + " to be quiescent, as it is a Headless or SAF-ingest project.");
+        long lastModified = 0L;
+        long oldLastModified = -1L;
+        long stableTime = 0L;
+        // if a document directory in a headless project hasn't been modified in the last 10 seconds, it's probably ready
+        while ((oldLastModified < lastModified) || (oldLastModified == lastModified) && (System.currentTimeMillis() - stableTime) < documentCreationWait) {
+            lastModified = directory.lastModified();
+            if ((lastModified != oldLastModified) || stableTime == 0L) {
+                stableTime = System.currentTimeMillis();
+            }
+            oldLastModified = lastModified;
+        }
+    }
+
+    private Document applyDefaultValues(Document document) {
+        for (MetadataFieldGroup mfg : document.getFields()) {
+            MetadataFieldValue mfv = new MetadataFieldValue();
+            mfv.setValue(mfg.getLabel().getProfile().getDefaultValue());
+            mfg.addValue(mfv);
+            document.addField(mfg);
+        }
+        return documentRepo.save(document);
+    }
+
+    private Document publishToRepositories(Document document) {
+        for (ProjectRepository repository : document.getProject().getRepositories()) {
+            try {
+                document = ((Repository) projectServiceRegistry.getService(repository.getName())).push(document);
+            } catch (IOException e) {
+                logger.error("Exception thrown attempting to push to " + repository.getName() + "!", e);
+                e.printStackTrace();
+            }
+        }
+        return document;
+    }
+
+    protected void addResource(File file) {
+        String documentName = file.getParentFile().getName();
+        List<String> documentPendingResources = pendingResources.get(documentName);
+        documentPendingResources.add(file.getAbsolutePath());
+    }
+
+}
