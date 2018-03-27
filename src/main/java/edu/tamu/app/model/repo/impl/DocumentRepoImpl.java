989860edfe50cb57fa26436fe37fc6f17a7627f5
James Creel <jcreel@library.tamu.edu>
2018-03-27 08:37:47 -0500

    JPA fixes

--- src/main/java/edu/tamu/app/model/repo/impl/DocumentRepoImpl.java
+++ src/main/java/edu/tamu/app/model/repo/impl/DocumentRepoImpl.java
@@ -2,8 +2,6 @@ package edu.tamu.app.model.repo.impl;
 
 import static edu.tamu.app.Initialization.ASSETS_PATH;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +9,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import edu.tamu.app.model.Document;
-import edu.tamu.app.model.MetadataFieldGroup;
-import edu.tamu.app.model.MetadataFieldValue;
 import edu.tamu.app.model.Project;
 import edu.tamu.app.model.repo.DocumentRepo;
-import edu.tamu.app.model.repo.MetadataFieldGroupRepo;
-import edu.tamu.app.model.repo.MetadataFieldValueRepo;
 import edu.tamu.app.model.repo.custom.DocumentRepoCustom;
 import edu.tamu.app.model.repo.specification.DocumentSpecification;
 import edu.tamu.weaver.data.model.repo.impl.AbstractWeaverRepoImpl;
@@ -25,12 +19,6 @@ public class DocumentRepoImpl extends AbstractWeaverRepoImpl<Document, DocumentR
 
     @Autowired
     private DocumentRepo documentRepo;
-
-    @Autowired
-    private MetadataFieldGroupRepo metadataFieldGroupRepo;
-
-    @Autowired
-    private MetadataFieldValueRepo metadataFieldValueRepo;
 
     @Override
     public synchronized Document create(Project project, String name, String path, String status) {
@@ -44,23 +32,6 @@ public class DocumentRepoImpl extends AbstractWeaverRepoImpl<Document, DocumentR
     @Override
     public Page<Document> pageableDynamicDocumentQuery(Map<String, String[]> filters, Pageable pageable) {
         return documentRepo.findAll(new DocumentSpecification<Document>(filters), pageable);
-    }
-
-    @Override
-    public Document fullSave(Document document) {
-        List<MetadataFieldGroup> mfgs = new ArrayList<MetadataFieldGroup>();
-        for (MetadataFieldGroup mfg : document.getFields()) {
-            List<MetadataFieldValue> mfvs = new ArrayList<MetadataFieldValue>();
-            for (MetadataFieldValue mfv : mfg.getValues()) {
-                mfv = metadataFieldValueRepo.save(mfv);
-                mfvs.add(mfv);
-            }
-            mfg.setValues(mfvs);
-            mfg = metadataFieldGroupRepo.save(mfg);
-            mfgs.add(mfg);
-        }
-        document.setFields(mfgs);
-        return documentRepo.update(document);
     }
 
     @Override
